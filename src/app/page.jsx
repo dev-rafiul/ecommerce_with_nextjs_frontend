e508977b0be52c4ae36110c77eca506c43f893d0
COMRADE040 <rafiulislam040@gmail.com>
2026-04-17 02:06:22 +0600

    initial setup and also created navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex min-h-screen items-center justify-center'>
+      <button className='btn btn-primary'>
+        Test Button
+      </button>
+      
+    </div>
+  )
+}
