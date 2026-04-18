81322f5390f563abcfa59bd431014baf845e339a
COMRADE040 <rafiulislam040@gmail.com>
2026-04-18 19:24:11 +0600

    daisyui navbar

---
+++ src/components/layouts/Logo.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <Link href={""}>
+            
+        </Link>
+    );
+};
+
+export default Logo;
