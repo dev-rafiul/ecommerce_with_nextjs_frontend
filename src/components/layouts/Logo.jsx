f08530f9b0d6c536d54d487b73a4a7b5d0ca24b8
COMRADE040 <rafiulislam040@gmail.com>
2026-04-18 19:32:34 +0600

    logo size adjust

--- src/components/layouts/Logo.jsx
+++ src/components/layouts/Logo.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <Link href={"/"}>
-            <Image alt='logo-hero-kidz' src={"/assets/logo.png"} width={80} height={70}/>
+        <Link className='flex items-center gap-1' href={"/"}>
+            <Image alt='logo-hero-kidz' src={"/assets/logo.png"} width={50} height={40}/>
             <h2 className='text-xl font-bold'>Hero Kidz</h2>
         </Link>
     );
