99e524c5664269a775c0ad2c00784ccba1cfdcc6
COMRADE040 <rafiulislam040@gmail.com>
2026-04-20 21:36:55 +0600

    set main min h screen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex min-h-screen items-center justify-center'>
-      <button className='btn btn-primary'>
-        Test Button
-      </button>
+    <div className=''>
+      
       
     </div>
   )
