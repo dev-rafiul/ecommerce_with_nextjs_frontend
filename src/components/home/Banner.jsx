2a477a2e7a46194e4050514c16a1d7796bd25bb9
COMRADE040 <rafiulislam040@gmail.com>
2026-04-21 23:04:03 +0600

    fontbangla setup for banner heading

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,3 +1,4 @@
+import { fontBangla } from '@/app/layout';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,7 +6,7 @@ const Banner = () => {
     return (
         <div className='flex justify-between items-center'>
             <div className='flex-1 space-y-5'>
-            <h2 className='text-4xl font-bold'>আপনার শিশু কে দিন একটি সুন্দর ভবিষ্যৎ </h2>
+            <h2 className={`${fontBangla.className} text-6xl font-bold`}>আপনার শিশু কে দিন একটি সুন্দর ভবিষ্যৎ </h2>
             <p>Buy Every Toy with up to 15% Discount</p>
             <button className='btn btn-primary btn-outline'>Explore Products</button>
             </div>
